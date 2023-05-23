496dfe8324be6f61c766156cead0a8b9442546d1
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-23 12:18:26 +0530

    added the react context hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import './App.css';
-import Counter from './state-management/Counter';
-import LoginStatus from './state-management/LoginStatus';
-import TaskList from './state-management/TaskList';
+import { useReducer } from "react";
+import "./App.css";
+import Counter from "./state-management/Counter";
+import LoginStatus from "./state-management/LoginStatus";
+import TaskList from "./state-management/TaskList";
+import tasksReducer from "./state-management/reducers/tasksReducer";
+import NavBar from "./state-management/NavBar";
+import HomePage from "./routing/HomePage";
+import TasksContext from "./state-management/contexts/tasksContext";
 
 function App() {
-  return <>
-    <Counter/>
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  return (
+    <>
+      {/* <Counter/>
     <LoginStatus/>
-    <TaskList/>
-  </>
+    <TaskList/> */}
+      <TasksContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <TaskList />
+      </TasksContext.Provider>
+    </>
+  );
 }
 
 export default App;
