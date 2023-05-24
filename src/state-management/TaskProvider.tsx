2f9a40b0525fb4ab2cae6c9c54bf3304e8416bad
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-24 11:47:16 +0530

    added custom providers

---
+++ src/state-management/TaskProvider.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode, useReducer } from "react";
+import TasksContext from "./contexts/tasksContext";
+import tasksReducer from "./reducers/tasksReducer";
+
+interface Props {
+  children: ReactNode;
+}
+
+const TaskProvider = ({ children }: Props) => {
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  return <TasksContext.Provider value={{ tasks, dispatch }}>{children}</TasksContext.Provider>;
+};
+
+export default TaskProvider;
