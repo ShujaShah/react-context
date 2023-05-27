6b1ea59691e3071ac3dfe70b0d6ae383c76a6f43
ShujaShah <shuja.ashraf11@gmail.com>
2023-05-27 16:04:57 +0530

    implemented react routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import router from './routing/routes';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 ).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}></RouterProvider>
   </React.StrictMode>
 );
